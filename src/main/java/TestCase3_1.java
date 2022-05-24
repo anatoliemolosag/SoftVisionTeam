8c3b95657dd6d45168119551dedd3660b2a6e471
Hana <h_huugii@yahoo.com>
2022-05-24 11:56:00 -0500

    Hi it's Hana I pushed my code

---
+++ src/main/java/TestCase3_1.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCase3_1 {
+@Test
+public void jewelryAisDropdownMenu (){
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.etsy.com/");
+	String actual = driver.getCurrentUrl();
+	String expected = "https://www.etsy.com/";
+	
+	WebElement checkJewelryAccessory = driver.findElement(By.id("catnav-primary-link-10855"));
+	
+	checkJewelryAccessory.isDisplayed();
+	Assert.assertEquals(actual,expected);
+}
+
+}
---
+++ src/main/java/TestCase3_2.java
@@ -0,0 +1,30 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class TestCase3_2 {
+@Test
+public void JewelryAccIsDisplayed () throws InterruptedException {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.etsy.com/");
+	Thread.sleep(3000);
+	WebElement checkDropdown = driver.findElement(By.id("//catnav-primary-link-10855"));
+	Thread.sleep(3000);
+	checkDropdown.isDisplayed();
+	List<WebElement> allCatergories = driver.findElements(By.xpath("//span[@class='wt-text-black']"));
+	List<String > names = new ArrayList<>();
+	Actions actions = new Actions(driver);
+	for (int i =0;i<allCatergories.size();i ++){
+		names.add(BrowserUtils.getText(allCatergories.get(i)));
+	}
+	Collections.sort(names);
+	System.out.println(names);
+	
+}
+}
