c91ef0987d31d7e088b5e221687fb66bb9786f8b
burulaidarmanova <burulaidarmanova@gmai.com>
2022-05-25 00:25:50 -0500

    Last code

--- src/main/java/TestCase6_1.java
+++ src/main/java/TestCase6_1.java
@@ -17,17 +17,12 @@ public class TestCase6_1 {
         WebElement clickCartButton=driver.findElement(By.xpath("//a[@aria-label='Cart']"));
         clickCartButton.click();
 
-
-
         WebElement headerOfCart=driver.findElement(By.xpath("//h1[contains(text(),'Your cart is empty.')]"));
         String actualHeader=headerOfCart.getText().trim();
         String expectedHeader="Your cart is empty.";
         Assert.assertEquals(actualHeader,expectedHeader);
 
-        WebElement headerOfDiscover=driver.findElement(By.xpath("//a[.='Discover something unique to fill it up']"));
-        String actualHeaderOfDiscover=headerOfDiscover.getText().trim();
-        String expectedHeaderOfDiscover="Discover something unique to fill it up";
-        Assert.assertEquals(actualHeaderOfDiscover,expectedHeaderOfDiscover);
+
 
 
     }
--- src/main/java/TestCase6_2.java
+++ src/main/java/TestCase6_2.java
@@ -1,2 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TestCase6_2 {
+    @Test
+
+    public void Validation() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.etsy.com/");
+
+        WebElement clickCartButton = driver.findElement(By.xpath("//a[@aria-label='Cart']"));
+        clickCartButton.click();
+        WebElement headerOfDiscover = driver.findElement(By.xpath("//a[.='Discover something unique to fill it up']"));
+        String actualHeaderOfDiscover = headerOfDiscover.getText().trim();
+        String expectedHeaderOfDiscover = "Discover something unique to fill it up";
+        Assert.assertEquals(actualHeaderOfDiscover, expectedHeaderOfDiscover);
+    }
 }
